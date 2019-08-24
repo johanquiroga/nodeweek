fde4c3e2e366592d88d1b272ae5c61ec13636ed5
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-08-24 13:05:09 -0500

    added new greeting function and added dynamic name support for greeting

--- handler.js
+++ handler.js
@@ -1,18 +1,30 @@
+/* eslint-disable strict */
+
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.hello = async ({ queryStringParameters }) => {
+  const name =
+    queryStringParameters && queryStringParameters.name
+      ? ` ${queryStringParameters.name}`
+      : '';
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: `Hello${name}! Your function executed successfully!`,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.helloJohan = async event => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    message: 'Hello Johan',
+  }),
+});
+
+// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
